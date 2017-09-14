664afee44442b4d252539bdaeb2c93f8b5aa9f66
Wan <e.wan@hotmail.it>
2017-09-14 13:04:38 +0200

    ADD: thread listen

---
+++ pds/test/Message.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Threading;
+
+namespace test
+{
+    class Message
+    {
+        const int BUF_LEN = 1024;
+        IPAddress mcastAddr;
+        int mcastPort;
+        IPAddress localAddr;
+        Socket mcastSocket;
+        MulticastOption mcastOption;
+        byte[] buffer = new byte[BUF_LEN];
+        EndPoint endP;
+        EndPoint localEnd;
+        IPEndPoint mcastEndP;
+        Boolean listening;
+
+        /*
+         * Message() 
+         * inizialization with creation of mcastSocket
+         */
+        public Message()
+        {
+            mcastAddr = IPAddress.Parse("224.168.100.3");
+            mcastPort = 11000;
+            mcastEndP = new IPEndPoint(mcastAddr, mcastPort);
+            mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            localAddr = IPAddress.Parse("172.22.52.14");
+            localEnd = (EndPoint)new IPEndPoint(localAddr, mcastPort);
+            mcastSocket.Bind(localEnd); // bind to local endpoint
+
+            mcastOption = new MulticastOption(mcastAddr);
+            mcastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, mcastOption); // add multicast address to mcastSocket
+            mcastSocket.MulticastLoopback = false;
+            Debug.WriteLine("multicast group: " + mcastOption.Group);
+            Debug.WriteLine("multicast local address: " + mcastOption.LocalAddress);
+        }
+
+        ~Message()
+        {
+            Debug.WriteLine("Message: destructor");
+            mcastSocket.Close();
+        }
+
+        /*
+         * Start()
+         * crea un nuovo thread che esegue la funzione ListenEveryone in background
+         */
+        public void Start()
+        {
+            var th = new Thread(ListenEveryone);
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        /*
+         * Stop()
+         * ferma l'esecuzione del thread in ascolto sul socket multicast
+         */
+        public void Stop()
+        {
+            // sezione critica
+            listening = false;
+            // fine sezione critica
+        }
+
+        /*
+         *  HelloEveryone()
+         *  manda un pacchetto UDP multicast 
+         *  messaggio: "Hello"
+         */
+        public void HelloEveryone()
+        {
+            Debug.WriteLine("Discover: start");
+            endP = new IPEndPoint(IPAddress.Any, 0);
+            mcastSocket.SendTo(ASCIIEncoding.ASCII.GetBytes("MandaFacile: Hello!"), mcastEndP);
+
+            Debug.WriteLine("Discover: end");
+        }
+
+        /*
+         * ListenEveryone()
+         * loop infinito per la ricezione di pacchetti UDP sull'indirizzo multicast
+         * e l'invio del pacchetto di presentazione ogni volta che arriva un nuovo utente
+         */
+        public void ListenEveryone()
+        {
+            // sezione critica
+            listening = true;
+            // fine sezione critica
+
+            this.HelloEveryone();
+            while (listening)
+            {
+                endP = new IPEndPoint(IPAddress.Any, 0);
+                Debug.WriteLine(endP.ToString());
+                mcastSocket.ReceiveFrom(buffer, ref endP);
+                if (!endP.ToString().Equals("172.22.52.14"))
+                {
+                    Debug.WriteLine(endP.ToString());
+                    Debug.WriteLine(Encoding.ASCII.GetString(buffer, 0, buffer.Length));
+
+                    // TO DO: inviare le informazione del nuovo utente tramite qualche sorta di 
+                    // canale di comunicazione al thread MainWindow
+
+                    // inviare il proprio pacchetto di presentazione in multicast
+                    // per farti conoscere dal nuovo utente 
+                    // e per aggiornare le info che gli altri utenti possiedono
+                    HelloEveryone();
+                }
+
+                endP = null;
+            }
+        }
+        /*
+                public void DecodeMessage(ref string msg)
+                {
+                    msg.Split('\n')
+                    if ()
+                    {
+
+                    }
+                }
+                */
+    }
+}
---
+++ pds/test/Network.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Diagnostics;
+
+
+namespace test
+{
+    class Network
+    {
+        /*
+         * Send "Hello!" message to everyone in the multicast group
+         */
+        static public void Hello()
+        {
+            Debug.WriteLine("inside Hello()");
+
+            IPAddress mcastAddr;
+            int mcastPort;
+            Socket mcastSocket;
+            IPEndPoint mcastEndP;
+            MulticastOption mcastOpt;
+
+            IPAddress localAddr;           
+            IPEndPoint localEndP;      
+           
+            byte[] msg = new byte[100];
+
+            mcastAddr = IPAddress.Parse("224.168.100.3");
+            mcastPort = 11000;
+            mcastEndP = new IPEndPoint(mcastAddr, mcastPort);
+            mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            localAddr = IPAddress.Parse("192.168.1.98");
+            localEndP = new IPEndPoint(localAddr, 0);
+
+            //mcastSocket.Bind(localEndP);
+
+            mcastOpt = new MulticastOption(mcastAddr);
+            mcastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, mcastOpt);
+
+            mcastSocket.SendTo(ASCIIEncoding.ASCII.GetBytes("Hello!"), mcastEndP);
+
+            EndPoint remoteEndP = new IPEndPoint(IPAddress.Any, 0);
+            mcastSocket.ReceiveFrom(msg, ref remoteEndP);
+            Console.WriteLine(Encoding.ASCII.GetString(msg, 0, msg.Length));
+
+            mcastSocket.Close();
+
+            return;
+        }
+            
+        
+        
+    }
+}
