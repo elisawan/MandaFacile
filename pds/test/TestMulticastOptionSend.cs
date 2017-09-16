b3fcf33b47804faaae1a8f9f83f7591fef2d38b7
Wan <e.wan@hotmail.it>
2017-09-16 11:56:58 +0200

    FIX: multicast

--- pds/test/TestMulticastOptionSend.cs
+++ pds/test/TestMulticastOptionSend.cs
@@ -30,9 +30,9 @@ namespace test
 
                 // Get the local IP address used by the listener and the sender to
                 // exchange multicast messages. 
-                // Console.Write("\nEnter local IPAddress for sending multicast packets: ");
-                // IPAddress localIPAddr = IPAddress.Parse(Console.ReadLine());
-                IPAddress localIPAddr = IPAddress.Any;
+                Console.Write("\nEnter local IPAddress for sending multicast packets: ");
+                IPAddress localIPAddr = IPAddress.Parse(Console.ReadLine());
+                // IPAddress localIPAddr = IPAddress.Any;
                 // Create an IPEndPoint object. 
                 IPEndPoint IPlocal = new IPEndPoint(localIPAddr, 0);
 
