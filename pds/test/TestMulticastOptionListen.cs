0884b734f88c58647e6d9d9d027708e48b3cfb9d
ElisaWan <elisa.zhimin.wan@gmail.com>
2017-09-18 13:09:21 +0200

    FIX

--- pds/test/TestMulticastOptionListen.cs
+++ pds/test/TestMulticastOptionListen.cs
@@ -4,24 +4,8 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-
-// This is the listener example that shows how to use the MulticastOption class. 
-// In particular, it shows how to use the MulticastOption(IPAddress, IPAddress) 
-// constructor, which you need to use if you have a host with more than one 
-// network card.
-// The first parameter specifies the multicast group address, and the second 
-// specifies the local address of the network card you want to use for the data
-// exchange.
-// You must run this program in conjunction with the sender program as 
-// follows:
-// Open a console window and run the listener from the command line. 
-// In another console window run the sender. In both cases you must specify 
-// the local IPAddress to use. To obtain this address run the ipconfig comand 
-// from the command line. 
-//  
 namespace test
 {
-
     public class TestMulticastOptionListen
     {
 
@@ -92,7 +76,7 @@ namespace test
                     mcastSocket.ReceiveFrom(bytes, ref remoteEP);
 
                     Console.WriteLine("Received broadcast from {0} :\n {1}\n",
-                      groupEP.ToString(),
+                      remoteEP.ToString(),
                       Encoding.ASCII.GetString(bytes, 0, bytes.Length));
 
 
