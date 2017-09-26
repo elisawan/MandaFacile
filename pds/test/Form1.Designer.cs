e16d119741716f4720540f13c6b89972d0dda21f
Andrea <andrea.grippi@yahoo.it>
2017-09-26 13:05:53 +0200

    aggiunta progress bar (da testare)

--- pds/test/Form1.Designer.cs
+++ pds/test/Form1.Designer.cs
@@ -33,8 +33,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.buttonOpzioniProfilo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -65,26 +63,6 @@
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(308, 264);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(389, 264);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // buttonOpzioniProfilo
             // 
             this.buttonOpzioniProfilo.Location = new System.Drawing.Point(13, 265);
@@ -101,8 +79,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(557, 299);
             this.Controls.Add(this.buttonOpzioniProfilo);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -118,8 +94,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button buttonOpzioniProfilo;
     }
 }
--- pds/test/SendFile.cs
+++ pds/test/SendFile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Net.Sockets;
 using System.Net;
+using System.Windows.Forms;
 
 namespace test
 {
@@ -25,11 +26,26 @@ namespace test
 
         void Send()
         {
+            ProgressBar pBar1 = new ProgressBar();
+            pBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            pBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            pBar1.Maximum = 8;
+            // Set the initial value of the ProgressBar.
+            pBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each step being performed
+            pBar1.Step = 1;
+
+
             IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(receiver.get_address()), 15000);
+            pBar1.PerformStep();
 
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            pBar1.PerformStep();
             Console.WriteLine("ip: " + receiver.get_address());
             client.Connect(ipEndPoint);
+            pBar1.PerformStep();
 
             string buffer;
             byte[] preBuf;
@@ -37,12 +53,17 @@ namespace test
 
             buffer = String.Format("R {0} 20", fileName);
             Console.WriteLine(buffer);
+            pBar1.PerformStep();
             preBuf = Encoding.ASCII.GetBytes(buffer);
+            pBar1.PerformStep();
             postBuf = Encoding.ASCII.GetBytes(_STRING_END_);
+            pBar1.PerformStep();
             client.SendFile(fileName, preBuf, postBuf, TransmitFileOptions.UseDefaultWorkerThread);
+            pBar1.PerformStep();
             Console.WriteLine("file sent");
 
             client.Shutdown(SocketShutdown.Both);
+            pBar1.PerformStep();
             client.Close();
         }
 
