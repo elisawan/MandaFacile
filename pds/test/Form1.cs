3f26acfabc35b5b3a7782fa56e21cce221340461
Andrea <andrea.grippi@yahoo.it>
2017-10-10 14:09:34 +0200

    ora ok

--- pds/test/Form1.cs
+++ pds/test/Form1.cs
@@ -244,20 +244,33 @@ namespace test
 
         public void IncreaseProgressBar(object sender, EventArgs e)
         {
-            // Increment the value of the ProgressBar a value of one each time.
-            progressBar1.Increment(progresso - progressBar1.Value);
-            // Display the textual value of the ProgressBar in the StatusBar control's first panel.
-            statusBar1.Text = progressBar1.Value.ToString() + "% Completed";
-            // Determine if we have completed by comparing the value of the Value property to the Maximum value.
-            if (progressBar1.Value == progressBar1.Maximum)
+
+            if (progresso == -1)
             {
                 // Stop the timer.
                 time.Stop();
-                MessageBox.Show("Invio completato!");
+                MessageBox.Show("Richiesta d'invio non accettata");
                 progressBar1.Value = 0;
                 progressBar1.Visible = false;
                 statusBar1.Text = "...";
                 buttonStop.Enabled = false;
+            }else
+            {
+                // Increment the value of the ProgressBar a value of one each time.
+                progressBar1.Increment(progresso - progressBar1.Value);
+                // Display the textual value of the ProgressBar in the StatusBar control's first panel.
+                statusBar1.Text = progressBar1.Value.ToString() + "% Completed";
+                // Determine if we have completed by comparing the value of the Value property to the Maximum value.
+                if (progressBar1.Value == progressBar1.Maximum)
+                {
+                    // Stop the timer.
+                    time.Stop();
+                    MessageBox.Show("Invio completato!");
+                    progressBar1.Value = 0;
+                    progressBar1.Visible = false;
+                    statusBar1.Text = "...";
+                    buttonStop.Enabled = false;
+                }
             }
         }
 
